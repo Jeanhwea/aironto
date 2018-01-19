af38abe9f4ec39994b521ee001def872a335f3a7
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-01-20 01:29:24 +0800

    fommatting

--- app/javascript/components/MainPage.js
+++ app/javascript/components/MainPage.js
@@ -5,15 +5,18 @@ const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
 class MainPage extends React.Component {
+
   state = {
     current: 'mail',
   }
+
   handleClick = (e) => {
     console.log('click ', e);
     this.setState({
       current: e.key,
     });
   }
+
   render() {
     return (
       <div style={{ padding: '24px' }}>
@@ -45,6 +48,7 @@ class MainPage extends React.Component {
       </div>
     );
   }
+
 }
 
 export default MainPage
