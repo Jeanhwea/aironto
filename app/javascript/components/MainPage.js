cf2baf34ad2ec775c203b1cb461ceec1937db594
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-01-20 00:26:36 +0800

    add main page

---
+++ app/javascript/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class MainPage extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Main Page</h1>
+      </div>
+    )
+  }
+}
+
+export default MainPage
