7f5ee0d9f0b2eef87b742bbd4c9dd6c95c499be4
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-01-23 21:53:37 +0800

    修改新增的文案

--- app/javascript/components/TopMenu.js
+++ app/javascript/components/TopMenu.js
@@ -40,7 +40,7 @@ class TopMenu extends React.Component {
           <MenuItem key="projects">
             <a href="/projects" rel="noopener noreferrer">项目列表</a>
           </MenuItem>
-          <SubMenu title={<span><Icon type="plus-circle-o" />新增元素</span>}>
+          <SubMenu title={<span><Icon type="plus-circle-o" />新增</span>}>
             <MenuItemGroup title="本体相关">
               <MenuItem key="new:concept">
                 <a href="/concepts/new" rel="noopener noreferrer">概念</a>
